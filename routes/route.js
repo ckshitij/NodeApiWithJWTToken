1c240dca03a43808436865ea93827a0f70dc3651
Kshitij Chaurasiya <Kshitij.Chaurasiya@veritas.com>
2018-08-28 21:53:16 +0530

    Adding request login and logout api with jwt token

---
+++ routes/route.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
+var config = require('../config');
+const User = require('../models/user.model');
+var verify_token = require('../Verifytoken');
+
+
+// Require the controllers WHICH WE DID NOT CREATE YET!!
+const controller = require('../controllers/controller');
+
+
+// a simple test url to check that all of our files are communicating correctly.
+router.get('/test', controller.test);
+router.post('/register',controller.register);
+router.post('/login',controller.login);
+
+//router.get('/me', verify_token, controller.verify);
+router.get('/logout',controller.logout);
+
+module.exports = router;
